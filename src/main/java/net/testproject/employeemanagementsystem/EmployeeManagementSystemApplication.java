20eb10ebefa47287706d42d8f0d56ee23ecfe260
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-08-02 18:10:58 +0530

    Build get all employees REST API

--- src/main/java/net/testproject/employeemanagementsystem/EmployeeManagementSystemApplication.java
+++ src/main/java/net/testproject/employeemanagementsystem/EmployeeManagementSystemApplication.java
@@ -1,13 +1,34 @@
 package net.testproject.employeemanagementsystem;
 
+import net.testproject.employeemanagementsystem.model.Employee;
+import net.testproject.employeemanagementsystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeManagementSystemApplication {
+public class EmployeeManagementSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = new Employee();
+		employee.setFirstName("Ramesh");
+		employee.setLastName("Fadatare");
+		employee.setEmailId("ramesh@gmail.com");
+		employeeRepository.save(employee);
+
+		Employee employee1 = new Employee();
+		employee1.setFirstName("John");
+		employee1.setLastName("Cena");
+		employee1.setEmailId("john.cenah@gmail.com");
+		employeeRepository.save(employee1);
+	}
 }

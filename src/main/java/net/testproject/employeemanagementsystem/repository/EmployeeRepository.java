950ee47fd9b3cb9f9991ecdeee7517d80ebfe6e0
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-08-02 17:30:42 +0530

    Create JPA entity, Repository & Exception

---
+++ src/main/java/net/testproject/employeemanagementsystem/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package net.testproject.employeemanagementsystem.repository;
+
+import net.testproject.employeemanagementsystem.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // all crud database methods
+}

f3c11efd1c81a4a0e002dc9f6aaab7ca8b08e815
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-08-05 03:41:47 +0530

    Configure routing in the react application

--- React_Js/react-hooks-frontend/src/App.js
+++ React_Js/react-hooks-frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
@@ -6,9 +7,16 @@ import ListEmployeeComponent from './components/ListEmployeeComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+            <Route path="/" element = {<ListEmployeeComponent />} />
+            <Route path="/employees" element = {<ListEmployeeComponent />} />   
+          </Routes>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }

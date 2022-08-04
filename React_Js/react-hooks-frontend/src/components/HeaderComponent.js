9ad80658e3377ce6a60b7973fa795b0121db98ed
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-08-05 02:53:49 +0530

    Add Header and Footer to react application

---
+++ React_Js/react-hooks-frontend/src/components/HeaderComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div>
+                        <a href="https://github.com/NaveendraKularatne" className="navbar-brand text-details">Employee Management Application</a>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default HeaderComponent
